d823b8c614152496149c8b89f4b50b16b6d1ca83
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2021-12-13 21:45:08 +0100

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+const isProduction = process.env.NODE_ENV === "production";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({
+        config: {
+          path: "./postcss.config.js",
+        },
+        extensions: [".css"],
+        extract: true,
+        minimize: isProduction,
+        // modules: true,
+      }),
+      isProduction && (await import("rollup-plugin-terser")).terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
