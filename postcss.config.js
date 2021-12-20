2985b7b3d8d9a60ed4e73dd501df20a2c9b716d8
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2021-12-20 21:44:19 +0100

    Fix wrong font in production (no need to purge css when using tailwind)

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
-const isProduction = process.env.NODE_ENV === "production";
-
 module.exports = {
   plugins: [
     require("postcss-preset-env"),
@@ -7,24 +5,5 @@ module.exports = {
     require("tailwindcss"),
     require("autoprefixer"),
     require("postcss-nested"),
-    ...(isProduction
-      ? [
-          require("@fullhuman/postcss-purgecss")({
-            // Specify the paths to all of the template files in your project
-            content: [
-              "./index.html",
-              "./src/**/*.tsx",
-              //"./src/**/*.html",
-              //"./src/**/*.vue",
-              //"./src/**/*.jsx",
-              // etc.
-            ],
-
-            // Include any special characters you're using in this regular expression
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          }),
-        ]
-      : []),
   ],
 };
