df5c8915acf0dd8934835dbe4240e68a789e5c60
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2021-12-22 20:57:29 +0100

    Run unit tests during CI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/node_modules/", "/generator-storybook"],
   moduleNameMapper: {
     ".(css|less|scss)$": "identity-obj-proxy",
   },
