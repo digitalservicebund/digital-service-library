a6467d716d0024fdf0c1f0a7952903c52ab9d8ce
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2022-01-25 12:11:00 +0300

    Add tests for Checkbox, Buttons and Icons

--- src/components/Icons/Icons.test.tsx
+++ src/components/Icons/Icons.test.tsx
@@ -2,11 +2,39 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import {
+  BulletIcon,
+  ChevronDownIcon,
   ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+  CircleIcon,
+  CloudIcon,
+  DownloadIcon,
+  ExternalIcon,
+  LinkIcon,
+  PencilIcon,
+  PlusIcon,
+  SearchIcon,
 } from "./index";
 
 describe("Icons", () => {
-  test("renders the ChevronLeft Icon", () => {
-    render(<ChevronLeftIcon />);
+  [
+    BulletIcon,
+    ChevronRightIcon,
+    ChevronLeftIcon,
+    ChevronDownIcon,
+    ChevronUpIcon,
+    CircleIcon,
+    CloudIcon,
+    DownloadIcon,
+    ExternalIcon,
+    LinkIcon,
+    PencilIcon,
+    PlusIcon,
+    SearchIcon,
+  ].forEach((Icon) => {
+    test(`renders the ${Icon.name} Icon`, () => {
+      render(<Icon />);
+    });
   });
 });
